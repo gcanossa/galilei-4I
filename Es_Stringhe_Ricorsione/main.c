3566eb628de73d0fe5b9f60eac90893c0f0594ea
Gianmaria Canossa <gianmaria.canossa@galileiostiglia.edu.it>
2020-10-21 13:09:13 +0200

    esempio operatore ternario

--- Es_Stringhe_Ricorsione/main.c
+++ Es_Stringhe_Ricorsione/main.c
@@ -3,10 +3,7 @@
 void stampa(char* s); // char[]
 
 int main(int argc, char** argv) { // char[][]
-    if(argc == 1)
-        printf("Ciao mondo\n");
-    else
-        printf("Ciao %s\n", argv[1]);
+    printf("Ciao %s\n", argc == 1 ? "mondo" : argv[1]);
 }
 
 void stampa(char* s){

a87656b5488f4f32cc074ba8fdb2ed427e91a0c9
Gianmaria Canossa <gianmaria.canossa@galileiostiglia.edu.it>
2020-10-21 13:05:22 +0200

    Inizio esempi argc/argv

---
+++ Es_Stringhe_Ricorsione/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void stampa(char* s); // char[]
+
+/*
+    command:    ./main pippo pluto
+    argv:
+    x    0 1 2 3 4 5 6 7 8
+    0    . / m a i n \0
+    1    p i p p o \0
+    2    p l u t o \0
+
+*/
+
+
+int main(int argc, char** argv) { // char[][]
+    int i;
+
+    printf("%d\n", argc);
+
+    for(i=0; i<argc; i++){
+        stampa(*(argv+i)); // argv[i]
+
+        printf("\n");
+    }
+}
+
+void stampa(char* s){
+    printf("%s", s);
+}

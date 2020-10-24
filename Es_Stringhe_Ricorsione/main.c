eca3d6cbabddaaaccc0ecd6f354b16351e1444db
Gianmaria Canossa <gianmaria.canossa@galileiostiglia.edu.it>
2020-10-24 09:46:43 +0200

    stampaRev corretta

--- Es_Stringhe_Ricorsione/main.c
+++ Es_Stringhe_Ricorsione/main.c
@@ -20,7 +20,7 @@ int main(int argc, char** argv) { // char[][]
     printf("%d\n", argc);
 
     for(i=0; i<argc; i++){
-        stampa(*(argv+i)); // argv[i]
+        stampaRev(*(argv+i)); // argv[i]
 
         printf("\n");
     }
@@ -37,7 +37,40 @@ void stampa(char* s){
 
 void stampaRev(char* s){
     if(*s!='\0'){
-        stampa(s+1);
+        stampaRev(s+1);
+        printf("%c",*s);
+    }
+}
+
+/*
+1) 'p' -> p
+    2) 'r' -> r
+        3) 'o' -> o
+            4) 'v' -> v
+                5) 'a' -> a
+                    6) '\0'
+                5)
+            4)
+        3)
+    2)
+1)
+
+1) 'p'
+    2) 'r'
+        3) 'o'
+            4) 'v'
+                5) 'a'
+                    6) '\0'
+                5) -> a
+            4) -> v
+        3) -> o
+    2) -> r
+1) -> p
+*/
+
+void stampaRev(char* s){
+    if(*s!='\0'){
+        stampaRev(s+1);
         printf("%c",*s);
     }
 }

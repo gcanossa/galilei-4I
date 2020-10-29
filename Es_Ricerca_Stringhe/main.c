37e72227f160f5168f6c36e151933afd4b90d34e
Gianmaria Canossa <gianmaria.canossa@galileiostiglia.edu.it>
2020-10-29 12:14:18 +0100

    cirrezione compito

--- Es_Ricerca_Stringhe/main.c
+++ Es_Ricerca_Stringhe/main.c
@@ -8,17 +8,39 @@ int countOccurrences(char* s, char* p);
 
 int main(int argc, char** argv)
 {
-    printf("Occorrenze di \"%s\" in \"%s\": %d\n", argv[1], *(argv+2), countOccurrences(argv[1], *(argv+2)));
+    // printf("%d\n", find(argv[1],argv[2]));
+    printf("Occorrenze di \"%s\" in \"%s\": %d\n", argv[2], *(argv+1), countOccurrences(argv[1], *(argv+2)));
 
     return 0;
 }
 
+int findR(char* s, char* p, char* c){ // ritorna -1 se p non è in s, altrimenti il suo indice
+    
+    if(*s=='\0' && *s!=*p){
+        return -1;
+    }
+    else if(*p=='\0'){
+        return 0;
+    }
+    else if(*s==*p){
+        return findR(s+1,p+1,c);
+    }
+    else{
+        int tmp = findR(s+1,c,c);
+
+        return tmp < 0 ? -1 : 1 + tmp;
+    }
+}
 int find(char* s, char* p){ // ritorna -1 se p non è in s, altrimenti il suo indice
-    // todo
-    return 0;
+    
+    return findR(s,p,p);
 }
 
 int countOccurrences(char* s, char* p){ // ritorna il numero di volte in cui p è contenuta in s
-    // finire
-    return 0;
+    
+    int idx = find(s,p);
+    if(idx<0)
+        return 0;
+    else
+        return 1 + countOccurrences(s+idx+1,p);
 }

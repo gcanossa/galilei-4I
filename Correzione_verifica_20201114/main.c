f094093dd1f43bdf7c5b875a29dafb101b54d06a
Gianmaria Canossa <gianmaria.canossa@galileiostiglia.edu.it>
2020-11-21 10:52:36 +0100

    correzione bis

--- Correzione_verifica_20201114/main.c
+++ Correzione_verifica_20201114/main.c
@@ -2,6 +2,10 @@
 
 int es1(char* a, char* b);
 int es2(char* a, char* b);
+int es3(int n);
+void es4(char*);
+void es5(char*);
+void es6();
 
 int main(int argc, char** argv){
 
@@ -11,6 +15,7 @@ int main(int argc, char** argv){
     return 0;
 }
 
+// a==b , a!=b , !(a==b && c!=d)
 int es1(char* a, char* b){
     int indiceTrovato =-1;
     int j = 0;
@@ -25,7 +30,16 @@ int es1(char* a, char* b){
         else{
             indiceTrovato=-1;
             k=0;
+
+            if(a[j]==*(b+k)){
+                if(indiceTrovato==-1)
+                    indiceTrovato=j;
+                k++;
+            }
         }
+
+        // if(a[j]!=b[k-1])... : per pinotti se vuol provare
+        
         j++;
     }
     
@@ -48,5 +62,81 @@ int es2(char* a, char* b){
     return indicePenultimaRicerca;
 }
 
+int es2_r(char* a, char* b){
+    
+    int idx = es1(a,b);
+    if(idx<0)
+        return -1;
+    else
+        return 1 + idx + es2_r(a+idx+1,b);
+}
+
+/*
+int fatt(int n){
+    if(n<=0)
+        return 1;
+    else
+        return n*fatt(n-1);
+}
+*/
+int es3(int n){
+    int cnt = 1;
+
+    while(n>=0){ // !(n<=0)
+        cnt = n * cnt;
+        n = n -1;
+    }
+
+    return cnt;
+}
+
+/*
+    void print(char* s){
+        if(*s!='\0'){
+            printf("%c", *s);
+            print(s+1);
+        }
+    }
+*/
+void es4(char* s){
+    if(*s!='\0'){
+        if(*s=='a' || *s=='e' || *s=='i' || *s=='o' || *s=='u'){
+            printf("%c", *s);
+        }
+        print(s+1);
+    }
+}
+void es5(char* s){
+    if(*s!='\0'){
+        if(*s=='a' || *s=='e' || *s=='i' || *s=='o' || *s=='u'){
+            printf("%c", *s);
+        }
+
+        print(s+1);
+
+        // (*s!='\0' && *s=='a') || *s=='e' || *s=='i' || *s=='o' || *s=='u'
+        // 5*3+2
+
+        if(!(*s=='a' || *s=='e' || *s=='i' || *s=='o' || *s=='u')){
+            printf("%c", *s);
+        }
+    }
+}
+
+void es6(){
+    int arr[3];
+    // [4]         [4]         [4]
+    // [] [] [] [] [] [] [] [] [] [] [] []
+
+    char* s = (char*)arr;
+
+    scanf("%s", s);
+    printf("%s", s);
+
+    for(int i=0;*s!='\0';i++){
+        printf("%c", s[i]);
+        printf("%c", *(s+i));
+    }
+}
 // ciao mondo iaa > aaiodnom oaic
 // ia > ia

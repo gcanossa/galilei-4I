561916a193f88afdb6e8ac59eb676c75b2849d4c
Gianmaria Canossa <gianmaria.canossa@galileiostiglia.edu.it>
2021-04-22 12:33:43 +0200

    file in java

--- Java_Files/Program.java
+++ Java_Files/Program.java
@@ -1,33 +1,47 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Program {
 
     public static void Step1(){
         try {
-            File fp = new File("esempio/elenco.txt");
+            File fp = new File("elenco.txt");
     
             if(fp.exists())
                 System.out.println("Esiste");
-            if(fp.isDirectory())
-                System.out.println("Si tratta di una cartella");
-            if(fp.isFile())
-                System.out.println("Si tratta di un file");
+            // if(fp.isDirectory())
+            //     System.out.println("Si tratta di una cartella");
+            // if(fp.isFile())
+            //     System.out.println("Si tratta di un file");
     
-            fp.mkdirs();
             fp.createNewFile();
     
             if(fp.exists())
                 System.out.println("Esiste");
+
+            FileWriter fw = new FileWriter(fp);
+
+            fw.write("<html>\n");
+            fw.write("<head>\n");
+
+            fw.close();
+
+            FileReader fr = new FileReader(fp);
+            Scanner sc = new Scanner(fr);
+
+            String x = sc.nextLine();
+
+            System.out.println(x);
     
             //fp.delete();
 
-            File fp2 = new File("esempio2/elenco2.txt");
-            fp2.mkdirs();
-            //fp2.createNewFile();
-            fp.renameTo(fp2);
+            // File fp2 = new File("esempio2/elenco2.txt");
+            // fp2.mkdirs();
+            // //fp2.createNewFile();
+            // fp.renameTo(fp2);
 
-            Exception x = new EsempioException("Errore di prova", 5);
-            throw x;
+            // Exception x = new EsempioException("Errore di prova", 5);
+            // throw x;
         }
         catch(ArithmeticException e){
             System.out.println(e.getMessage() + " " + e.getStackTrace()[0].getLineNumber());
